f2c9260b718e83f323ef7169f11a5c0848c794a5
lei wang <wanglei1369@hotmail.com>
2020-04-10 05:14:55 +1200

    first commit

---
+++ src/pages/App.tsx
@@ -0,0 +1,38 @@
+/** @format */
+
+import React, {Suspense} from "react"
+import "../components/i18n"
+import Layout from "../layouts/layout"
+import Service from "../components/Service"
+import Banner from "../components/Banner"
+import Offices from "../components/Offices"
+import Terminal from "../components/Terminal"
+import Slider from "../components/Slider"
+import FMA from "../components/FMA"
+import PriceTable from "../components/PriceTable"
+
+function IndexPageComponent() {
+    return (
+        <>
+            <Layout>
+                <Banner />
+                <Service />
+                <PriceTable />
+                <FMA />
+                <Slider />
+                <Terminal />
+                <Offices></Offices>
+            </Layout>
+        </>
+    )
+}
+
+function IndexPage() {
+    return (
+        <Suspense fallback="loading">
+            <IndexPageComponent />
+        </Suspense>
+    )
+}
+
+export default IndexPage
